59571e5db7e6fa27d0fe6bbef2d8703e53a3199f
Jesus Zozaya <jesus@voltera.io>
2016-07-08 03:20:20 -0400

    Increased stepper timer from 60 seconds to 5 minutes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -175,7 +175,7 @@
 #define INVERT_E_STEP_PIN false
 
 //default stepper release if idle
-#define DEFAULT_STEPPER_DEACTIVE_TIME 60
+#define DEFAULT_STEPPER_DEACTIVE_TIME (5*60)
 
 #define DEFAULT_MINIMUMFEEDRATE       0.0     // minimum feedrate
 #define DEFAULT_MINTRAVELFEEDRATE     0.0
