e4b6da79f8a67cd1bc3c34e3924f745c7c0a00e8
Troy Gonsalves <troy@voltera.io>
2016-10-05 00:37:12 -0400

    Add an R (retract) option to V4 (probe)

--- Marlin/compensationAlgorithms/api.h
+++ Marlin/compensationAlgorithms/api.h
@@ -2,4 +2,4 @@ void applyScalingCompensation(float& x, float& y, float xScale, float yScale);
 
 void applySkewCompensation(float& x, float& y, float cosTheta, float tanTheta);
 
-long applyBacklashCompensation(int direction, long steps, long offset);
+long applyBacklashCompensation(const char* label, int direction, long steps, long offset);
