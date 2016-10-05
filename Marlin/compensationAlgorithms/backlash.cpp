7853722336dd49a1557d13ac99426ead7d1b59b0
Troy Gonsalves <troy@voltera.io>
2016-10-05 00:38:51 -0400

    Add some labels to help with debugging

--- Marlin/compensationAlgorithms/backlash.cpp
+++ Marlin/compensationAlgorithms/backlash.cpp
@@ -1,11 +1,11 @@
 #include "../Marlin.h" // for logging
 
-long applyBacklashCompensation(int direction, long steps, long offset) {
+long applyBacklashCompensation(const char* label, int direction, long steps, long offset) {
   long compensatedSteps = steps + (direction * offset);
 
   if (logging_enabled) {
     SERIAL_ECHO_START;
-    SERIAL_ECHOPGM("Applying backlash compensation: ");
+    SERIAL_ECHOPGM("Applying backlash compensation to "); SERIAL_ECHO(label); SERIAL_ECHOPGM(": ");
     SERIAL_ECHO(steps);
     SERIAL_ECHOPGM(" + ");
     SERIAL_ECHO(direction);
