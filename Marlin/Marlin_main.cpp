9857a1355925be1cddf65081fe20abb5e69c8a85
Jesus Zozaya <jesus@voltera.io>
2016-11-14 15:12:06 -0500

    Version bump to 0.3.0

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -46,7 +46,7 @@
 #include "src/commands/processing.h"
 
 
-#define VERSION_STRING  "v0.2.0"
+#define VERSION_STRING  "v0.3.0"
 
 // look here for descriptions of G-codes: http://linuxcnc.org/handbook/gcode/g-code.html
 // http://objects.reprap.org/wiki/Mendel_User_Manual:_RepRapGCodes
