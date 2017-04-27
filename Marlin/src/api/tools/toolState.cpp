e91327e7feea5c8e7d68b6a69c8afe9b1814daff
Troy Gonsalves <troy@voltera.io>
2017-04-27 12:46:21 -0400

    fix classify a mounted unpowered router as ‘unmounted’ (not probe)

--- Marlin/src/api/tools/toolState.cpp
+++ Marlin/src/api/tools/toolState.cpp
@@ -11,12 +11,17 @@ static enum ToolStates s_classifyVoltage(Tool, float voltage) {
     SERIAL_ECHO_START;
     SERIAL_ECHOPGM("Classifying voltage "); SERIAL_ECHOLN(voltage);
   }
-  if (voltage < 1.0) {
+  if (voltage < 1.0) { // expected 0.7 (once stable)
     return TOOL_STATE_TRIGGERED;
-  } else if (voltage <= 3.0) {
-    return TOOL_STATE_ROUTER_MOUNTED;
-  } else if (voltage <= 4.0) {
+  } else if (voltage <= 3.2) {
+    // Note: ROUTER gives 2.06 when mounted and not powered
+    // we don't treat this as a unique state right now, but
+    // we don't want to clarify this as a PROBE.
+    return TOOL_STATE_NOT_MOUNTED;
+  } else if (voltage <= 3.8) { // expecting 3.55 (once stable)
     return TOOL_STATE_PROBE_MOUNTED;
+  } else if (voltage <= 4.5) { // expecting 4.22 (once stable)
+    return TOOL_STATE_ROUTER_MOUNTED;
   } else {
     return TOOL_STATE_NOT_MOUNTED;
   }
