f9ef36e30b74433a9d4b085740477ddd2b910a09
Troy Gonsalves <troy@voltera.io>
2017-10-04 15:00:39 -0400

    Move all PTopPin code to h-file

--- Marlin/src/vone/pins/PTopPin/PTopPin.h
+++ Marlin/src/vone/pins/PTopPin/PTopPin.h
@@ -5,6 +5,8 @@
 #include "../../../utils/rawToVoltage.h"
 #include "../adc/SamplingHelper.h"
 
+#include "CustomSerial.h"
+
 class PTopPin {
   public:
     struct Sample {
@@ -21,18 +23,21 @@ class PTopPin {
       }
     };
 
-    PTopPin(int digitalPin, int analogPin);
+    inline PTopPin(int digitalPin, int analogPin);
 
-    FORCE_INLINE int analogPin() { return _analogPin; }
+    FORCE_INLINE int analogPin() const { return _analogPin; }
 
-    FORCE_INLINE Sample voltage();
-    FORCE_INLINE Sample readVoltage();
+    // Returns the most recently sampled value
+    FORCE_INLINE Sample value();
+
+    // Blocks until a new sample is available, then returns it
+    FORCE_INLINE Sample readValue();
 
     FORCE_INLINE int readDigitalValue(bool& value);
 
-    int resetTool();
+    inline int resetTool();
 
-    int send(char* msg);
+    inline int send(char* msg);
 
     // ADC sampling
     FORCE_INLINE bool tryStartAdcSampling();
@@ -52,13 +57,22 @@ class PTopPin {
     };
     volatile Mode mode = Mode::Idle;
 
-    static const char* modeToString(Mode mode);
+    static const char* modeToString(Mode mode) {
+      switch(mode) {
+        case Mode::Idle: return "Idle";
+        case Mode::Communication: return "Communication";
+        case Mode::ResetTool: return "Reset Tool";
+        case Mode::AdcSamplng: return "Analog Sampling";
+        case Mode::DirectRead: return "Direct Read";
+      }
+      return "Unknown";
+    };
 
     FORCE_INLINE void setMode_Idle();
     FORCE_INLINE bool tryToSetMode_AdcSampling();
     FORCE_INLINE bool trytoSetMode_DirectRead();
-    bool trytoSetMode_Communication();
-    bool trytoSetMode_ResetTool();
+    inline bool trytoSetMode_Communication();
+    inline bool trytoSetMode_ResetTool();
 
     // ADC sampling
     volatile unsigned long preventAdcSamplingUntil = 0;
@@ -69,9 +83,18 @@ class PTopPin {
     FORCE_INLINE bool _readDigitalValue();
 
     // Communication
-//     CustomSerial serial;
-    long commandSentTime = 0;
+    CustomSerial customSerial;
 };
+
+PTopPin::PTopPin(int digitalPin, int analogPin)
+  : _digitalPin(digitalPin)
+  , _analogPin(analogPin)
+  , adcSamples(numSamples)
+  , customSerial(digitalPin)
+{
+  // Reset attached tool, if there is one
+  resetTool();
+}
 
 // ----------------------------------------------
 // Idle
@@ -80,9 +103,69 @@ class PTopPin {
 void PTopPin::setMode_Idle() {
   ScopedInterruptDisable sid;
   if (mode != Mode::Idle) {
-    digitalWrite(_digitalPin, 1);
+    digitalWrite(_digitalPin, HIGH);
     mode = Mode::Idle;
   }
+}
+
+// ----------------------------------------------
+// Reset tool
+
+bool PTopPin::trytoSetMode_ResetTool() {
+  ScopedInterruptDisable sid;
+  if (mode == Mode::Idle) {
+    SERIAL_ECHO_START;
+    SERIAL_ECHOLNPGM("Resetting tool");
+    digitalWrite(_digitalPin, LOW);
+    mode = Mode::ResetTool;
+    return true;
+  }
+  return false;
+}
+
+int PTopPin::resetTool() {
+  while (trytoSetMode_ResetTool()) {
+    delay(1);
+  }
+  delay(500);
+  setMode_Idle();
+  return 0;
+}
+
+
+// ----------------------------------------------
+// Communication
+
+bool PTopPin::trytoSetMode_Communication() {
+  ScopedInterruptDisable sid;
+  if (mode == Mode::Idle) {
+    mode = Mode::Communication;
+    return true;
+  }
+  return false;
+}
+
+int PTopPin::send(char* msg) {
+  while (!trytoSetMode_Communication()) {
+    delay(1);
+  }
+
+  const unsigned maxAttempts = 3;
+  unsigned numAttempts = 0;
+  if (customSerial.send(msg, maxAttempts, &numAttempts)) {
+    return -1;
+  }
+
+  setMode_Idle();
+
+  if (numAttempts > 2) {
+    SERIAL_ECHO_START;
+    SERIAL_PAIR("NOTICE: p-top message sent on attempt ", numAttempts);
+    SERIAL_PAIR(" of ", maxAttempts);
+    SERIAL_EOL;
+  }
+
+  return 0;
 }
 
 // ----------------------------------------------
@@ -182,5 +265,5 @@ int PTopPin::readDigitalValue(bool& value) {
   }
 }
 
-PTopPin::Sample PTopPin::voltage()     { return Sample( adcSamples.value()     ); }
-PTopPin::Sample PTopPin::readVoltage() { return Sample( adcSamples.readValue() ); }
+PTopPin::Sample PTopPin::value()     { return Sample( adcSamples.value()     ); }
+PTopPin::Sample PTopPin::readValue() { return Sample( adcSamples.readValue() ); }
