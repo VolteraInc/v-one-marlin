203e5671f17cd59061b5d2e4db7db7c6940d6453
Troy Gonsalves <thetroy@gmail.com>
2017-12-19 17:27:40 -0500

    Optimize Heater to eliminate missed Serial characters
    Disabling interrupts then retrieving the latest temperature was (somehow) taking too long. Perhaps the memory allocations?
    It's not necessary to disable interrupts to retrieve that value, so removing the 'disable' fixed the missing characters (bad checksums) issue.
    
    Also tweaked the bounds check to return if out-of-bound...otherwise the heater control code could turn the heater back on.

--- Marlin/src/vone/bed/heater/Heater.h
+++ Marlin/src/vone/bed/heater/Heater.h
@@ -8,8 +8,8 @@ class Heater {
   public:
     inline Heater(HeaterPin& heatPin, BedTemperaturePin &tempPin);
 
-    float currentTemperature() { ScopedInterruptDisable sid; return currentTemp; };
-    float targetTemperature() { ScopedInterruptDisable sid; return targetTemp; };
+    float currentTemperature() { return currentTemp; };
+    float targetTemperature() { return targetTemp; };
     inline void setTargetTemperature(float target);
 
     bool isHeating() { ScopedInterruptDisable sid; return currentTemp < targetTemp; };
@@ -24,8 +24,7 @@ class Heater {
     volatile float currentTemp = 0.0f;
     volatile float targetTemp = 0.0f;
 
-    inline void _updateTemperature();
-    inline void _updateHeating();
+    inline void updateHeating(float temperature);
 };
 
 Heater::Heater(HeaterPin& heatPin, BedTemperaturePin &tempPin)
@@ -43,32 +42,21 @@ void Heater::setTargetTemperature(float target) {
   SERIAL_EOL;
 };
 
-void Heater::_updateTemperature() {
-  // Get temperature
-  const auto temperature = temperaturePin.value().temperature;
-
-  // Update volatile member
-  {
-    ScopedInterruptDisable sid;
-    currentTemp = temperaturePin.value().temperature;
-  }
-
+void Heater::updateHeating(float temperature) {
   // Shut off the heater if the temperature is out of range
   // Notes: A temp below the minimum suggests the thermometer is broken
-  if ( temperature < BED_MINTEMP || temperature > BED_MAXTEMP) {
+  if (temperature < BED_MINTEMP || temperature > BED_MAXTEMP) {
     SERIAL_ERROR_START;
-    SERIAL_PAIR("Unable to complete heating, current temperature is ", temperature);
+    SERIAL_PAIR("Unable to operate heater, current temperature is ", temperature);
     SERIAL_PAIR(" degrees, which is outside of the expected range, ", BED_MINTEMP);
     SERIAL_PAIR(" to ", BED_MAXTEMP);
     SERIAL_EOL;
     heaterPin.stopHeating();
+    return;
   }
-}
 
-void Heater::_updateHeating() {
   // Turn heater on/off
-  ScopedInterruptDisable sid;
-  if (currentTemp < targetTemp) {
+  if (temperature < targetTemp) {
     heaterPin.startHeating();
   } else {
     heaterPin.stopHeating();
@@ -76,16 +64,15 @@ void Heater::_updateHeating() {
 }
 
 void Heater::frequentInterruptibleWork() {
-  // Run every 0.5s starting after 3s
-  // Note: We wait 5s so we have time to collect a valid sample
-  //       otherwise we'd need to check endTime != 0
-  static unsigned long nextCheckAt = 5000;
+  // Run every 0.5s
+  static unsigned long nextCheckAt = 0;
   const auto now = millis();
   if (now < nextCheckAt) {
     return;
   }
   nextCheckAt = now + 500;
 
-  _updateTemperature();
-  _updateHeating();
+  // Get temperature then update heater
+  currentTemp = temperaturePin.value().temperature;
+  updateHeating(currentTemp);
 }
