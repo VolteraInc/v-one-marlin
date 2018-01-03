5be2591ab873baeecef3e2e6f762583bd65850bb
Troy Gonsalves <thetroy@gmail.com>
2018-01-03 11:09:44 -0500

    Stepper - add comments to add/stop

--- Marlin/src/vone/stepper/Stepper.cpp
+++ Marlin/src/vone/stepper/Stepper.cpp
@@ -19,14 +19,20 @@ void Stepper::resume() {
 
 int Stepper::add(float x, float y, float z, float e, float f) {
 
+  // Check if we are stopped
   if (m_stopped) {
     return -1;
   }
 
   plan_buffer_line(x, y, z, e, f / 60);
 
+  // Check again
+  // Note: the stepper may have been stopped from an interrupt handler
+  // (which is the case for tool detached), while normal execution was in
+  // plan_buffer_line(). So, we check again, and remove the point we just
+  // added, if necessary.
   if (m_stopped) {
-    stop(); // remove the point we just added
+    quickStop();
     return -1;
   }
 
