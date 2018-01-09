a26c825cb7915fcf153ee580a6f2b58b23154d38
Troy Gonsalves <thetroy@gmail.com>
2018-01-09 15:43:04 -0500

    VOne - better comments on frequentInterruptibleWork

--- Marlin/src/vone/VOne.h
+++ Marlin/src/vone/VOne.h
@@ -24,13 +24,24 @@ class VOne {
 
     // Perform work that must happen frequently but can be
     // interrupted (briefly) by time critical work like
-    // serial character reads and stepper work
+    // serial character reads and the stepper.
+    // Note: work done in the main loop can be delayed delayed by
+    //       10s, 30s or longer. It depends on how long it takes
+    //       to process the current command.
     inline void frequentInterruptibleWork() {
       // Allow other interrupts
       CBI(TIMSK0, OCIE0B);
       sei();
 
+      // Updated ADC values are needed by heater and toolDetector
       adc.frequentInterruptibleWork();
+
+      // Heater management
+      // Note: Delaying heater updates (even for a few seconds) could
+      //       result in several degrees of overshoot
+      // TODO: temperature profiles should be processed here
+      //       otherwise the next step in the profile can be delayed
+      //       by command processing
       heater.frequentInterruptibleWork();
 
       SBI(TIMSK0, OCIE0B);
