0fb6284198ac2a2774a1bd8c5da764e96f926951
Troy Gonsalves <thetroy@gmail.com>
2018-02-06 16:57:35 -0500

    vcode - add missing newline to error msg

--- Marlin/src/commands/vCode.cpp
+++ Marlin/src/commands/vCode.cpp
@@ -226,7 +226,7 @@ int process_vcode(int command_code) {
       using namespace probing;
       if (!code_seen('D')) {
         SERIAL_ERROR_START;
-        SERIAL_ERROR("Unable to probe hole, no diameter given");
+        SERIAL_ERRORLN("Unable to probe hole, no diameter given");
         return -1;
       }
       const auto holeDiameter = code_value();
