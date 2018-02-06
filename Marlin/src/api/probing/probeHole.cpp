79557e5dfaf27ff92ba6801ad98178f0fdb2b025
Troy Gonsalves <thetroy@gmail.com>
2018-02-06 15:24:12 -0500

    Probing - add probeHole()

---
+++ Marlin/src/api/probing/probeHole.cpp
@@ -0,0 +1,50 @@
+#include "probing.h"
+#include "../movement/movement.h"
+#include "../../vone/tools/Probe.h"
+#include "../../../serial.h"
+
+int probing::probeHole(
+  tools::Probe& probe,
+  const Point2d& center,
+  float radius,
+  Point3d measurements[],
+  unsigned int maxMeasurements,
+  unsigned int* o_numMeasurements,
+  unsigned int minPoints,
+  float probePitch
+) {
+  // Determine number of measurements
+  const auto circumference = TWO_PI * radius;
+  const unsigned int numMeasurements = max(
+    minPoints,
+    floor(circumference/probePitch)
+  );
+
+  // Confirm we have enough room for the measurements
+  if (numMeasurements > maxMeasurements) {
+    return -1;
+  }
+
+  // Probe around the hole
+  // Note: we start at the top of the hole
+  Point2d start = center.offset(0, -radius - 0.200);
+  const auto thetaPerSegment = TWO_PI / numMeasurements;
+  for (auto idx = 0u; idx < numMeasurements; ++idx) {
+    const auto dest = start.rotate(idx * thetaPerSegment, center);
+    measurements[idx].x = dest.x;
+    measurements[idx].y = dest.y;
+    if (
+      probe.moveToSafeHeight() ||
+      moveXY(probe, dest.x, dest.y) ||
+      probe.probe(measurements[idx].z)
+    ) {
+      return -1;
+    }
+  }
+
+  if (o_numMeasurements) {
+    *o_numMeasurements = numMeasurements;
+  }
+
+  return 0;
+}
--- Marlin/src/api/probing/probing.h
+++ Marlin/src/api/probing/probing.h
@@ -4,8 +4,28 @@ namespace tools {
   class Probe;
 }
 
-struct FeatureMeasurements {
-  // TODO
-};
+class Point2d;
+class Point3d;
 
-int probeFeature(tools::Probe& probe, FeatureMeasurements& featureMeasurements);
+namespace probing {
+
+  // ----------------------------------------------
+  // Probe Hole
+
+  namespace ProbeHole {
+    const auto DefaultMinPoints = 3u;
+    const auto DefaultProbePitch = 1.0;
+  }
+
+  int probeHole(
+    tools::Probe& probe,
+    const Point2d& center,
+    float radius,
+    Point3d measurements[],
+    unsigned int maxMeasurements,
+    unsigned int* o_numMeasurements = nullptr,
+    unsigned int minPoints = ProbeHole::DefaultMinPoints,
+    float probePitch = ProbeHole::DefaultProbePitch
+  );
+
+} // namespace probing
