66593a43efd79f6a8b9c30cf5b7165be1aca65b0
Troy Gonsalves <thetroy@gmail.com>
2018-08-07 10:36:55 -0400

    stepper - move digipots code to own file

--- Marlin/stepper.cpp
+++ Marlin/stepper.cpp
@@ -27,9 +27,9 @@ and Philipp Tiefenbacher. */
 #include "Configuration.h"
 #include "planner.h"
 #include "speed_lookuptable.h"
-#include <SPI.h>
 
 #include "src/vone/VOne.h"
+#include "src/vone/stepper/digipots.h"
 
 //===========================================================================
 //=============================public variables  ============================
@@ -671,52 +671,4 @@ void quickStop() {
   }
   current_block = NULL;
   ENABLE_STEPPER_DRIVER_INTERRUPT();
-}
-
-void digiPotInit() { //Initialize Digipot Motor Current
-  const uint8_t digipot_motor_current[] = DIGIPOT_MOTOR_CURRENT;
-  SPI.begin();
-  pinMode(DIGIPOTSS_PIN, OUTPUT);
-
-    // Cycle the SS pin - make sure digiPot initializes correctly.
-  digitalWrite(DIGIPOTSS_PIN,LOW);
-  digitalWrite(DIGIPOTSS_PIN,HIGH);
-
-  for (int i = 0; i < 4; ++i) {
-    digiPotSetCurrent(i, digipot_motor_current[i]);
-  }
-}
-
-void digiPotSetCurrent(uint8_t axis, uint8_t current) {
-  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
-  digiPotWrite(digipot_addrs[axis], current);
-}
-
-uint8_t digiPotGetCurrent(uint8_t axis) {
-  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
-  return digiPotRead(digipot_addrs[axis]);
-}
-
-// Refer to http://ww1.microchip.com/downloads/en/DeviceDoc/22242A.pdf
-// To get understanding of write/read codes and addresses.
-
-void digiPotWrite(uint8_t address, uint8_t value) {
-  address = address + 0x00; // 0x00 adds 'write' code. (no action here...)
-  SPI.beginTransaction(SPISettings(3000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(DIGIPOTSS_PIN,LOW); // take the SS pin low to select the chip
-  SPI.transfer(address); //  send in the address and value via SPI:
-  SPI.transfer(value);
-  digitalWrite(DIGIPOTSS_PIN,HIGH); // take the SS pin high to de-select the chip:
-  SPI.endTransaction();
-}
-
-uint8_t digiPotRead(uint8_t address) {
-  address =  address + 0x0C; // 0x0C adds 'read' code.
-  SPI.beginTransaction(SPISettings(3000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(DIGIPOTSS_PIN,LOW); // take the SS pin low to select the chip
-  SPI.transfer(address);
-  uint8_t val = SPI.transfer(0x00); // Send dummy to clock in value.:
-  digitalWrite(DIGIPOTSS_PIN,HIGH); // take the SS pin high to de-select the chip:
-  SPI.endTransaction();
-  return val;
 }
--- Marlin/stepper.h
+++ Marlin/stepper.h
@@ -62,10 +62,4 @@ void checkStepperErrors(); //Print errors detected by the stepper
 
 void quickStop();
 
-void digiPotInit();
-void digiPotSetCurrent(uint8_t axis, uint8_t current);
-uint8_t digiPotGetCurrent(uint8_t axis);
-void digiPotWrite(uint8_t address, uint8_t value);
-uint8_t digiPotRead(uint8_t address);
-
 #endif
