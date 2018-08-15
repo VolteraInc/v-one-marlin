e2c972c61151cb130721e6fe7ce06590bbd0b8c5
Troy Gonsalves <thetroy@gmail.com>
2018-08-14 23:25:59 -0400

    fix compile of digipots

--- Marlin/src/vone/stepper/digipots.cpp
+++ Marlin/src/vone/stepper/digipots.cpp
@@ -4,31 +4,6 @@
 #include "../../../Configuration.h"
 #include "../../../pins.h"
 
-// Initialize Digipot Motor Current
-void digiPotInit() {
-  const uint8_t digipot_motor_current[] = DIGIPOT_MOTOR_CURRENT;
-  SPI.begin();
-  pinMode(DIGIPOTSS_PIN, OUTPUT);
-
-  // Cycle the SS pin - make sure digiPot initializes correctly.
-  digitalWrite(DIGIPOTSS_PIN,LOW);
-  digitalWrite(DIGIPOTSS_PIN,HIGH);
-
-  for (int i = 0; i < 4; ++i) {
-    digiPotSetCurrent(i, digipot_motor_current[i]);
-  }
-}
-
-void digiPotSetCurrent(uint8_t axis, uint8_t current) {
-  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
-  s_digiPotWrite(digipot_addrs[axis], current);
-}
-
-uint8_t digiPotGetCurrent(uint8_t axis) {
-  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
-  return digiPotRead(digipot_addrs[axis]);
-}
-
 // Refer to http://ww1.microchip.com/downloads/en/DeviceDoc/22242A.pdf
 // To get understanding of write/read codes and addresses.
 
@@ -51,4 +26,29 @@ static uint8_t s_digiPotRead(uint8_t address) {
   digitalWrite(DIGIPOTSS_PIN,HIGH); // take the SS pin high to de-select the chip:
   SPI.endTransaction();
   return val;
+}
+
+// Initialize Digipot Motor Current
+void digiPotInit() {
+  const uint8_t digipot_motor_current[] = DIGIPOT_MOTOR_CURRENT;
+  SPI.begin();
+  pinMode(DIGIPOTSS_PIN, OUTPUT);
+
+  // Cycle the SS pin - make sure digiPot initializes correctly.
+  digitalWrite(DIGIPOTSS_PIN,LOW);
+  digitalWrite(DIGIPOTSS_PIN,HIGH);
+
+  for (int i = 0; i < 4; ++i) {
+    digiPotSetCurrent(i, digipot_motor_current[i]);
+  }
+}
+
+void digiPotSetCurrent(uint8_t axis, uint8_t current) {
+  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
+  s_digiPotWrite(digipot_addrs[axis], current);
+}
+
+uint8_t digiPotGetCurrent(uint8_t axis) {
+  const uint8_t digipot_addrs[] = DIGIPOT_ADDRESS;
+  return s_digiPotRead(digipot_addrs[axis]);
 }
--- Marlin/src/vone/stepper/digipots.h
+++ Marlin/src/vone/stepper/digipots.h
@@ -5,5 +5,3 @@
 void digiPotInit();
 void digiPotSetCurrent(uint8_t axis, uint8_t current);
 uint8_t digiPotGetCurrent(uint8_t axis);
-void digiPotWrite(uint8_t address, uint8_t value);
-uint8_t digiPotRead(uint8_t address);
