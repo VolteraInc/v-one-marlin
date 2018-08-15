841b46c2709bd29f2d69a501f4fe317e358ab076
Troy Gonsalves <thetroy@gmail.com>
2018-08-15 17:33:46 -0400

    TriggerEventLog - ring buffer was not advancing correctly

--- Marlin/src/vone/endstops/TriggerEventLog.h
+++ Marlin/src/vone/endstops/TriggerEventLog.h
@@ -27,18 +27,18 @@ class TriggerEventLog {
     inline bool empty() const { return size() == 0; }
     inline bool full() const { return size() == MAX_ENTRIES; }
 
-    inline const TriggerEvent& front() const { return m_log[m_frontIdx]; }
-    inline const TriggerEvent& back() const { return m_log[m_backIdx]; }
+    inline const TriggerEvent* front() const { return empty() ? nullptr : &m_log[m_frontIdx]; }
+    inline const TriggerEvent* back() const { return empty() ? nullptr : &m_log[m_backIdx]; }
 
     inline void push(const Endstop& endstop, float positionInAxis);
     inline void pop();
 
   private:
-    static const unsigned int MAX_ENTRIES = 12;
+    static const unsigned int MAX_ENTRIES = 10;
     TriggerEvent m_log[MAX_ENTRIES];
     volatile unsigned int m_size = 0;
     unsigned int m_frontIdx = 0;
-    unsigned int m_backIdx = 0;
+    unsigned int m_backIdx = MAX_ENTRIES - 1;
 
     static inline unsigned int increment(unsigned int idx) {
       return idx == MAX_ENTRIES - 1 ? 0 : idx + 1;
@@ -48,25 +48,22 @@ class TriggerEventLog {
 void TriggerEventLog::push(const Endstop& endstop, float positionInAxis) {
   ScopedInterruptDisable sid;
 
-  // Advance front and back
-  // Note: if we are empty the front and back already point where we need them to be.
-  if (!empty()) {
-    if (full()) {
-      // Drop
-      logWarning
-        << F("Too many trigger events, dropping event for")
-        << endstop.name
-        << F("at ")
-        << axis_codes[endstop.axis]
-        << F("=")
-        << positionInAxis
-        << endl;
-      return;
-    }
-    m_backIdx = increment(m_backIdx);
+  // Drop if full
+  if (full()) {
+    // Drop
+    logWarning
+      << F("Too many trigger events, dropping event for")
+      << endstop.name
+      << F("at ")
+      << axis_codes[endstop.axis]
+      << F("=")
+      << positionInAxis
+      << endl;
+    return;
   }
 
   // Store event
+  m_backIdx = increment(m_backIdx);
   m_log[m_backIdx] = { endstop, positionInAxis };
   ++m_size;
 }
@@ -74,8 +71,11 @@ void TriggerEventLog::push(const Endstop& endstop, float positionInAxis) {
 void TriggerEventLog::pop() {
   ScopedInterruptDisable sid;
 
-  if (!empty()) {
-    m_frontIdx = increment(m_frontIdx);
-    --m_size;
+  // Ignore if empty
+  if (empty()) {
+    return;
   }
+
+  --m_size;
+  m_frontIdx = increment(m_frontIdx);
 }
