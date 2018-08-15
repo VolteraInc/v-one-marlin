de960e85fd97b2ccb43d8d7706f3775519c1002b
Troy Gonsalves <thetroy@gmail.com>
2018-08-14 23:28:22 -0400

    algo - add fns to reverse compensations
    for converting steps back to a position

--- Marlin/src/compensationAlgorithms/api.h
+++ Marlin/src/compensationAlgorithms/api.h
@@ -1,5 +1,8 @@
 void applyScalingCompensation(float& x, float& y, float xScale, float yScale);
+float reverseScalingCompensation(float p, float axisScalingFactor);
 
 void applySkewCompensation(float& x, float& y, float cosTheta, float tanTheta);
+float reverseSkewCompensationInX(float x, float cosTheta);
+float reverseSkewCompensationInY(float x, float y, float tanTheta);
 
 long applyBacklashCompensation(const char* label, int direction, long steps, long offset);
--- Marlin/src/compensationAlgorithms/skew.cpp
+++ Marlin/src/compensationAlgorithms/skew.cpp
@@ -1,4 +1,14 @@
+#include "../../MarlinConfig.h"
+
 void applySkewCompensation(float& x, float& y, float cosTheta, float tanTheta) {
   y = (y - x * tanTheta);
   x = x / cosTheta;
+}
+
+float reverseSkewConpensationInX(float x, float cosTheta) {
+  return x * cosTheta;
+}
+
+float reverseSkewConpensationInY(float x, float y, float tanTheta) {
+  return y + x * sin(atan(tanTheta));
 }
