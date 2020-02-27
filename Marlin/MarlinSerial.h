5c8825afbe024da4221c2b5810c44662d6ed6dc1
Troy Gonsalves <thetroy@gmail.com>
2020-02-27 13:17:25 -0500

    Eliminate undefined behavior compile warning warning by removing UART_PRESENT macro
    using defined() in a macro has undefined behavior

--- Marlin/MarlinSerial.h
+++ Marlin/MarlinSerial.h
@@ -38,11 +38,6 @@
   #define SERIAL_PORT 0
 #endif
 
-// The presence of the UBRRH register is used to detect a UART.
-#define UART_PRESENT(port) ((port == 0 && (defined(UBRRH) || defined(UBRR0H))) || \
-                            (port == 1 && defined(UBRR1H)) || (port == 2 && defined(UBRR2H)) || \
-                            (port == 3 && defined(UBRR3H)))
-
 // These are macros to build serial port register names for the selected SERIAL_PORT (C preprocessor
 // requires two levels of indirection to expand macro values properly)
 #define SERIAL_REGNAME(registerbase,number,suffix) SERIAL_REGNAME_INTERNAL(registerbase,number,suffix)
