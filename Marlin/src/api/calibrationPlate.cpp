94eb4fb54f97b47210d1cf34769ce03328ef8254
Troy Gonsalves <thetroy@gmail.com>
2020-02-28 10:56:28 -0500

    enable toolswitch while contacting calibration plate (to detect errors)

--- Marlin/src/api/calibrationPlate.cpp
+++ Marlin/src/api/calibrationPlate.cpp
@@ -4,13 +4,20 @@
 // #include "../../stepper.h"
 #include "../vone/tools/Probe.h"
 #include "../vone/VOne.h"
+#include "../vone/endstops/ScopedEndstopEnable.h"
 
 static const float MinDisplacement = 0.050f;
 static const float MaxDisplacement = 0.500f;
 
 static int s_measureCalibrationPlateZ(float& plateZ, float maxTravel) {
-  log << F("Measuring calibration plate") << endl;
   const auto& calibrationPlate = vone->endstops.calibrationPlate;
+  const auto& toolSwitch = vone->endstops.toolSwitch;
+  auto& endstopMonitor = vone->stepper.endstopMonitor;
+
+  // Enable toolswitch, it should not trigger
+  ScopedEndstopEnable scopedEnable(endstopMonitor, toolSwitch);
+
+  log << F("Measuring calibration plate") << endl;
   return (
     measureAtSwitch(calibrationPlate, maxTravel, plateZ) ||
     retractFromSwitch(calibrationPlate)
