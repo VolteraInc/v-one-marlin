e1ac11e276f522807c80a34b9ecb4d05e8531b95
Troy Gonsalves <thetroy@gmail.com>
2020-03-09 13:35:21 -0400

    update calibrate switches to use raise/move/measure instead of homeZ + explicitly disable the toolSwitch
    also, set xypos z
    
    homeZ can not be used directly from here anymore because the toolSwitch would not be disabled, also
    we don't need a full home of Z.

--- Marlin/src/api/diagnostics/calibrateSwitchPositions.cpp
+++ Marlin/src/api/diagnostics/calibrateSwitchPositions.cpp
@@ -2,8 +2,14 @@
 #include "../../../Marlin.h"
 #include "../../../ConfigurationStore.h"
 #include "../../vone/tools/Probe.h"
+#include "../../vone/VOne.h"
+#include "../../vone/endstops/ScopedEndstopEnable.h"
 
 static int s_calibrateSwitchPositions(tools::Probe& probe, unsigned cycles, bool storeResults) {
+  auto& endstopMonitor = vone->stepper.endstopMonitor;
+  auto& toolSwitch = vone->endstops.toolSwitch;
+  auto& zSwitch = vone->endstops.zSwitch;
+
   if (probe.prepareToMove(tools::PrepareToMove::Option::skipCalibrateXYZ)) {
     logError << F("Unable to calibrate positions, could not prepare probe") << endl;
     return -1;
@@ -19,28 +25,34 @@ static int s_calibrateSwitchPositions(tools::Probe& probe, unsigned cycles, bool
   if (xyPositionerFindCenter(probe, cycles, centerX, centerY, usePlateBackOffForZ)) {
     return -1;
   }
+  float backoffPositionZ = current_position[Z_AXIS];
 
-  // Set the x,y position of the z-switch using hardcoded offset values.
-  min_z_x_pos = centerX + OFFSET_FROM_XYPOS_TO_MINZ_X;
-  min_z_y_pos = centerY + OFFSET_FROM_XYPOS_TO_MINZ_Y;
-
-  // Home Z (uses the new z-switch location)
-  if (homeZ(probe)) {
+  // Compute the position of the z-swith, then test the value
+  // Note: disable the tool switch becuase z-switches with
+  //       strong springs will cause it to trigger
+  float zSwitchX = centerX + OFFSET_FROM_XYPOS_TO_MINZ_X;
+  float zSwitchY = centerY + OFFSET_FROM_XYPOS_TO_MINZ_Y;
+  float zSwitchMeasurement;
+  ScopedEndstop_DISABLE sed(endstopMonitor, toolSwitch);
+  if (
+    raiseToEndstop() ||
+    moveXY(probe, zSwitchX, zSwitchY) ||
+    measureAtSwitch(zSwitch, useDefaultMaxTravel, zSwitchMeasurement)
+  ) {
+    logError << F("Unable to calibrate positions, could not test computed position of z-switch") << endl;
     return -1;
   }
 
-  // Find the center, using the standard algorithm
-  if (xyPositionerFindCenter(probe, cycles, centerX, centerY, useConfiguredZ)) {
-    return -1;
-  }
 
-  // Set the x,y position of the xy-positioner
-  xypos_x_pos = centerX;
-  xypos_y_pos = centerY;
 
-  // Store positions
+  // Store computed positions
   if (storeResults) {
     log << F("Storing settings...") << endl;
+    min_z_x_pos = zSwitchX;
+    min_z_y_pos = zSwitchY;
+    xypos_x_pos = centerX;
+    xypos_y_pos = centerY;
+    xypos_z_pos = backoffPositionZ - zSwitchMeasurement;
     Config_StoreCalibration();
   }
 
