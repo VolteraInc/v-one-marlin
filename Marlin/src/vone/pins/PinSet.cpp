ca260da41df6737b41e5715c8ced1fb9b045c204
EvanSkelsey <evanrdskelsey@gmail.com>
2024-08-27 09:27:16 -0400

    LED fix and version bump 1.7.2

--- Marlin/src/vone/pins/PinSet.cpp
+++ Marlin/src/vone/pins/PinSet.cpp
@@ -15,21 +15,12 @@ PinSet::PinSet(
   , heater(heaterDigitalPin)
 {
   // LED Pins
-  //SET_OUTPUT(LED_RED_PIN);
-  //WRITE(LED_RED_PIN, LOW);
-  pinMode(LED_RED_PIN, OUTPUT);
-  digitalWrite(LED_RED_PIN, LOW);
+  SET_OUTPUT(LED_RED_PIN);
+  WRITE(LED_RED_PIN, LOW);
 
+  SET_OUTPUT(LED_GREEN_PIN);
+  WRITE(LED_GREEN_PIN, LOW);
 
-  //SET_OUTPUT(LED_GREEN_PIN);
-  //WRITE(LED_GREEN_PIN, LOW);
-  pinMode(LED_GREEN_PIN, OUTPUT);
-  digitalWrite(LED_GREEN_PIN, LOW);
-
-
-  //SET_OUTPUT(LED_BLUE_PIN);
-  //WRITE(LED_BLUE_PIN, LOW);
-  pinMode(LED_BLUE_PIN, OUTPUT);
-  digitalWrite(LED_BLUE_PIN, LOW);
-
+  SET_OUTPUT(LED_BLUE_PIN);
+  WRITE(LED_BLUE_PIN, LOW);
 }
